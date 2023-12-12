9e6befcdce7237820bf4cfc66a7fab3053248457
Pavel Tsikhanau <tishka1991@gmail.com>
2023-12-13 00:31:24 +0300

    fix: fix export error

--- coffee-house/scripts/index.js
+++ coffee-house/scripts/index.js
@@ -1,4 +1,4 @@
-// import menu from '../scripts/menu.js';
+import {menu} from './menu.js';
 
 const burgerMenu = document.querySelector('.popup-burger-menu');
 const burgerMenuOpen = document.querySelector('.header-menu-button');
@@ -13,149 +13,6 @@ burgerMenuOpen.addEventListener('click', function() {
 burgerMenuClose.addEventListener('click', function() {
   burgerMenu.classList.add('hidden')
 });
-
-const menu = [
-  {
-    image: 'coffee-1.png',
-    name: 'Irish coffee',
-    description: 'Fragrant black coffee with Jameson Irish whiskey and whipped milk',
-    price: '7.00',
-    type: 'coffee',
-  },
-  {
-    image: 'coffee-2.png',
-    name: 'Kahlua coffee',
-    description: 'Classic coffee with milk and Kahlua liqueur under a cap of frothed milk',
-    price: '7.00',
-    type: 'coffee',
-  },
-  {
-    image: 'coffee-3.png',
-    name: 'Honey raf',
-    description: 'Espresso with frothed milk, cream and aromatic honey',
-    price:  '5.50',
-    type: 'coffee',
-  },
-  {
-    image: 'coffee-4.png',
-    name: 'Ice cappuccino',
-    description: 'Cappuccino with soft thick foam in summer version with ice',
-    price: '5.00',
-    type: 'coffee',
-  },
-  {
-    image: 'coffee-5.png',
-    name: 'Espresso',
-    description: 'Classic black coffee',
-    price: '4.50',
-    type: 'coffee',
-  },
-  {
-    image: 'coffee-6.png',
-    name: 'Latte',
-    description: 'Espresso coffee with the addition of steamed milk and dense milk foam',
-    price: '5.50',
-    type: 'coffee',
-  },
-  {
-    image: 'coffee-7.png',
-    name: 'Latte macchiato',
-    description: 'Espresso with frothed milk and chocolate',
-    price: '5.50',
-    type: 'coffee',
-  },
-  {
-    image: 'coffee-8.png',
-    name: 'Coffee with cognac',
-    description: 'Fragrant black coffee with cognac and whipped cream',
-    price: '6.50',
-    type: 'coffee',
-  },
-  {
-    image: 'tea-1.png',
-    name: 'Moroccan',
-    description: 'Fragrant black tea with the addition of tangerine, cinnamon, honey, lemon and mint',
-    price: '4.50',
-    type: 'tea',
-  },
-  {
-    image: 'tea-2.png',
-    name: 'Ginger',
-    description: 'Original black tea with fresh ginger, lemon and honey',
-    price: '5.00',
-    type: 'tea',
-  },
-  {
-    image: 'tea-3.png',
-    name: 'Cranberry',
-    description: 'Invigorating black tea with cranberry and honey',
-    price: '5.00',
-    type: 'tea',
-  },
-  {
-    image: 'tea-4.png',
-    name: 'Sea buckthorn',
-    description: 'Toning sweet black tea with sea buckthorn, fresh thyme and cinnamon',
-    price: '5.50',
-    type: 'tea',
-  },
-  {
-    image: 'dessert-1.png',
-    name: 'Marble cheesecake',
-    description: 'Philadelphia cheese with lemon zest on a light sponge cake and red currant jam',
-    price: '3.50',
-    type: 'dessert',
-  },
-  {
-    image: 'dessert-2.png',
-    name: 'Red velvet',
-    description: 'Layer cake with cream cheese frosting',
-    price: '4.00',
-    type: 'dessert',
-  },
-  {
-    image: 'dessert-3.png',
-    name: 'Cheesecakes',
-    description: 'Soft cottage cheese pancakes with sour cream and fresh berries and sprinkled with powdered sugar',
-    price: '4.50',
-    type: 'dessert',
-  },
-  {
-    image: 'dessert-4.png',
-    name: 'Creme brulee',
-    description: 'Delicate creamy dessert in a caramel basket with wild berries',
-    price: '4.00',
-    type: 'dessert',
-  },
-  {
-    image: 'dessert-5.png',
-    name: 'Pancakes',
-    description: 'Tender pancakes with strawberry jam and fresh strawberries',
-    price: '4.50',
-    type: 'dessert',
-  },
-  {
-    image: 'dessert-6.png',
-    name: 'Honey cake',
-    description: 'Classic honey cake with delicate custard',
-    price: '4.50',
-    type: 'dessert',
-  },
-  {
-    image: 'dessert-7.png',
-    name: 'Chocolate cake',
-    description: 'Cake with hot chocolate filling and nuts with dried apricots',
-    price: '5.50',
-    type: 'dessert',
-  },
-  {
-    image: 'dessert-8.png',
-    name: 'Black forest',
-    description: 'A combination of thin sponge cake with cherry jam and light chocolate mousse',
-    price: '6.50',
-    type: 'dessert',
-  },
-];
 
 const generateCards = (type) => {
   const desiredItems = menu.filter((item) => {
@@ -224,4 +81,6 @@ teaButton.addEventListener('click', function() {
 document.addEventListener("DOMContentLoaded", () => {
   repaintMenu('coffee');
 });
+
+
 
